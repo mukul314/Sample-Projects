947705f7aba7bbab31d8e7bd0fb89afafc17ec4c
Mukul Ojha <mukulsharma3779@gmail.com>
2022-06-23 16:13:44 +0530

    Add files via upload

---
+++ p.3 login form(shaking effect)/script.js
@@ -0,0 +1,12 @@
+const form = document.querySelector("form"),
+eField = form.querySelector(".email"),
+eInput = eField.querySelector("input"),
+pField = form.querySelector(".password"),
+pInput = pField.querySelector("input")
+
+form.onsubmit = (e) =>{
+    e.preventDefault();
+    if(eInput.value ==""){
+        eField.classList.add("shake");
+    }
+}
